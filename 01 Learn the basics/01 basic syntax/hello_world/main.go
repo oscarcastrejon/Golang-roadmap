dc666bf632d2131d9adb012f46600c5e659c2912
Oscar Castrejon <oscar.does.6@gmail.com>
2023-10-03 13:34:24 -0600

    hello_world -> basic syntax -> learn the basics. Roadmap Golang

---
+++ 01 Learn the basics/01 basic syntax/hello_world/main.go
@@ -0,0 +1,21 @@
+// https://gobyexample.com/hello-world
+
+// Our first program will print the classic “hello world” message.
+// Here’s the full source code.
+
+// To run it.
+// go run main.go
+
+// To build it. (will create a binary)
+// go build main.go
+
+// To execute the binary.
+// ./main
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello world!")
+}
